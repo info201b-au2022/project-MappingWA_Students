722cb041633ec0d901c4618db8400906a5e21fa2
adoraapham <apham25@uw.edu>
2022-11-14 15:59:25 -0800

    chart changes

--- source/graduation_rates.R
+++ source/graduation_rates.R
@@ -1,16 +1,27 @@
-library(tidyverse)
+library("tidyverse")
+rm(list = ls())
 
 report_data <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-MappingWA_Students/main/data/ospi_report_card_data/Report_Card_Graduation_2019-20.csv")
+View(report_data)
 
 graduation_rates <- report_data %>%
-  select(County, StudentGroup, GraduationRate)
-view(graduation_rates)
-
-library(tidyr)
-
-print(graduation_rates %>% drop_na())
+  filter(
+    str_detect(StudentGroupType, "Race")
+  )
 View(graduation_rates)
 
+graduation_rates <- graduation_rates %>%
+  select(County, StudentGroup, GraduationRate)
+View(graduation_rates)
+
+graduation_rates <- graduation_rates %>% drop_na()
+View(graduation_rates)
+
+graduation_rates <- graduation_rates %>%
+  filter(
+    !str_detect(StudentGroup, "Hispanic")
+  )
+View(graduation_rates)
 
 plot <- ggplot(data = graduation_rates) +
   geom_col(mapping = aes(
