7bc0528d019a3fae69306ea009e6af04c7613aa9
euchtmann <ellauchtmann@gmail.com>
2022-11-14 19:56:12 -0800

    adding povertyandrace summary variables

--- source/summary.R
+++ source/summary.R
@@ -1,6 +1,24 @@
-source("source/graduation_rates.R")
 library(tidyverse)
 library(dplyr)
+
+# CHART 2 - Ella
+#source("source/race_and_poverty.R")
+# What race has the highest percentage of people below the poverty level in Whitman county?
+View(census_data)
+
+most_below_poverty_whitman <- census_data %>%
+  filter(percent == max(percent, na.rm = TRUE)) %>%
+  pull(race)
+most_below_poverty_whitman
+
+# What race has the lowest percentage of people below the poverty level in Whitman county?
+least_below_poverty_whitman <- census_data %>%
+  filter(percent == min(percent, na.rm = TRUE)) %>%
+  pull(race)
+least_below_poverty_whitman
+
+# CHART 3 - Adora
+source("source/graduation_rates.R")
 
 report_data <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-MappingWA_Students/main/data/ospi_report_card_data/Report_Card_Graduation_2019-20.csv")
 View(report_data)
@@ -29,4 +47,6 @@ lowest_grad_rate <- report_data %>%
   filter(GraduationRate ==min(GraduationRate, na.rm=TRUE)) %>%
   pull(GraduationRate)
 print(lowest_grad_rate)
+
+
 
