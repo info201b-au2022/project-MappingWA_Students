ea39530794af5cd9c52ee0aaf3e1e18998d6943e
euchtmann <ellauchtmann@gmail.com>
2022-12-06 13:52:48 -0800

    combined all dataframes not working on slight changes to pie chart

--- source/race_and_poverty.R
+++ source/race_and_poverty.R
@@ -121,6 +121,11 @@ race_data_2020 <- census_data_2020 %>%
   select(year, race, counties, percent)
 View(race_data_2020)
 
+# Combine all data frames of the different years
+
+
+census_data <- rbind(race_data_2016, race_data_2017, race_data_2018, race_data_2019, race_data_2020)
+View(census_data)
 
 
 --------------------------------------------------------------------------------
