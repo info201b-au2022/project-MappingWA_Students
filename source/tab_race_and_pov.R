b7d31a628860b0749aedd78407f3aa44d851cb51
euchtmann <ellauchtmann@gmail.com>
2022-12-07 13:44:07 -0800

    explanation of data and how chart works need to add the takeaways once chart is seen

--- source/tab_race_and_pov.R
+++ source/tab_race_and_pov.R
@@ -1,22 +1,11 @@
 # working page for shiny web app for race and poverty pie chart 
 
 # to access data wrangling
-source("race_and_poverty.R")
-
-
-# # what is the average percentage of all racial groups in the year 2019
-# test_2019 <- census_data %>%
-#   mutate(percent_num = as.numeric(percent)) %>%
-#   select(year, race, percent_num) %>%
-#   filter(year == "2019") %>%
-#   group_by(race) %>%
-#   summarize(avg_percent = mean(percent_num, na.rm = TRUE))
-# View(test_2019)
+source("../source/race_and_poverty.R")
 
 # What goes in the UI
 
 year <- c("2016", "2017", "2018", "2019", "2020")
-
 year_select_box <- selectInput(
   "year", 
   label = "Select Year of Interest", 
@@ -25,18 +14,39 @@ year_select_box <- selectInput(
 )
 
 
-# What goes on my PAGE MAKE THIS INTO A FUNCTION
-tabPanel("Race and Poverty Pie Chart", 
+tabPanel("Chart 3", 
          sidebarLayout(
            sidebarPanel(
              year_select_box
            ),
            mainPanel(plotlyOutput("pieChart"),
-                     p(""),
+                     h3("Race and Poverty Pie Chart"),
+                     h4("The Data"),
+                     p("This chart represents the proportions of the average percentages 
+                       of the different racial groups below the poverty line. From the original 
+                       dataset sourced from the Census Bureau, the categories of Some other race alone, 
+                       Hispanic or Latino origin (of any race), White alone, not Hispanic or Latino, 
+                       were excluded as to strictly focus on racial groups, and not ethnicities, that 
+                       may hold influence on our analysis of the correlation between poverty and race.
+                       The measure at focus is the estimated percent of peoples below the poverty line within
+                       each racial group. The poverty line or threshold, is determined by a number
+                       of factors according to the Census Bureau. These factors include, but are not limted to, 
+                       family size, age of family members, total family income, child support, and rent. This 
+                       threshold updates every year due to inflation so it is important to understand that 
+                       when looking at data from different years, where the poverty theshold lies will vary."),
+                     h4("The Chart"),
+                     p("The proportions, or sections, of the chart are based on calculating the average percentage 
+                       of the race below the poverty line through different counties in Washington within a 
+                       specific year. Users have the ablity to see how the chart varies by year by selecting one of 
+                       the year options between 2016 and 2020 using the year selector."), 
+                     h4("The Takeaway"), 
                      p(""))
          )
 )
 
+
+
+# What goes in Server
 
 server <- function(input, output){
   output$pieChart <- renderPlotly({
@@ -54,9 +64,14 @@ server <- function(input, output){
         width = 1
       ) + 
       coord_polar("y", start = 0) +
-      labs(title = "Total Carbon Dioxide Emission from Production Including Land-Use Change", 
-           x = "Year", 
-           y = "Production Emissions of CO2 in Million Tonnes") + 
+      labs(
+           x = "", 
+           y = "", 
+           title = "Average Proportion of Racial Groups Below the Poverty Line by Year",
+           subtitle = "Based on Calculated Average Percent Values", 
+           caption = "MappingWA_Students Project", 
+           alt = "Average Proportion of Racial Groups Below the Poverty Line by Year"
+      ) + 
       theme(
         axis.text = element_blank()
       )
